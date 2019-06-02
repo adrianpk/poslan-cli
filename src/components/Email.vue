fec2a4402583c5395debfda157502d9ad608ef4a
Adrian K <adrian@kuguar.dev>
2019-06-02 23:27:00 +0200

    Initial commit.

---
+++ src/components/Email.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-layout column>Send an email</v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "email",
+  components: {}
+};
+</script>
+
+<style lang="stylus" scoped>
+.container {
+  @media only screen and (min-width: 1264px) {
+    max-width: 600px;
+  }
+
+  @media only screen and (min-width: 960px) {
+    max-width: 800px;
+  }
+}
+</style>
